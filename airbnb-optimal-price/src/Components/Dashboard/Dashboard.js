7b7cfef3e900eebe5830c8cfeb8bc96f362a69a0
git-austinmccollom <austinmccollom@gmail.com>
2020-09-21 22:05:14 -0500

    Dashboard and AddListing components

--- airbnb-optimal-price/src/Components/Dashboard/Dashboard.js
+++ airbnb-optimal-price/src/Components/Dashboard/Dashboard.js
@@ -1,13 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+
+//components
+import AddListing from './AddListing';
+
+
+//material UI
+import { AppBar, Toolbar, Typography, Button, Grid } from "@material-ui/core";
 
 export default function Dashboard(props) {
-    
-    return(
-        <div>
-            <h1>Dashboard</h1>
-            <h2>Add Listing</h2>
-            <form>
-            </form>
-        </div>
-    )
+  return (
+    <Grid container>
+      <Grid item xs={false} sm={2} />
+      <Grid item container xs={12} sm={8} justify='center'>
+        <Typography variant='h1'>Dashboard</Typography>
+        <AddListing />
+      </Grid>
+      <Grid item xs={false} sm={2} />
+    </Grid>
+  );
 }
